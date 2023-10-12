d605c7a60d4f02770306cf2f210bcf3a95c55499
William Fank Thomé <willegp88@gmail.com>
2023-10-12 11:23:34 -0300

    tmp: add extra info about query compilation

--- src/dsl/adapter/ebank_dsl_adapter_mnesia.erl
+++ src/dsl/adapter/ebank_dsl_adapter_mnesia.erl
@@ -11,5 +11,9 @@
 
 query(Clauses, Indexes) ->
     _Query = lists:flatten(ebank_qlc:query(mnesia, Clauses, Indexes)).
-    % @todo: compile the query.
+    % @todo: find a mechanism to compile the query.
+    %        The current implementation via parse_transform
+    %        just compiles the string and not the qlc QH.
+    %        @see: ebank_db_adapter_mnesia:read/2
+    %        @see: https://www.erlang.org/doc/man/qlc
     % ebank_qlc:compile(Query, [{'Bindings', Bindings}]).
